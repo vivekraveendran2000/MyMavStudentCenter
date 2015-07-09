b9cafc668eb97e000f3bebb410d2427d8f38b171
vivek <vivekraveendran2000@gmail.com>
2015-07-09 16:59:12 -0500

    Added back button and search button in search_input

--- app/src/main/java/uta/com/search/SearchInput.java
+++ app/src/main/java/uta/com/search/SearchInput.java
@@ -2,19 +2,43 @@ package uta.com.search;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import uta.com.studentcenter.R;
 
 /**
  * Created by vivekraveendran on 7/7/2015.
  */
-public class SearchInput extends Activity{
+public class SearchInput extends Activity implements View.OnClickListener{
 
+    ImageButton searchBtn,backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_input);
+        initViews();
+    }
+
+    public  void initViews(){
+
+        searchBtn = (ImageButton) findViewById(R.id.btn_search_input);
+        searchBtn.setOnClickListener(this);
+        backBtn = (ImageButton) findViewById(R.id.btn_search_input_back);
+        backBtn.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.equals(searchBtn)){
+
+        }else if (v.equals(backBtn)) {
+            finish();
+        }
     }
 }
