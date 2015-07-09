81864f6b990f9e13e635c9d8bbc070cd4104e56d
vivek <vivekraveendran2000@gmail.com>
2015-07-09 16:40:55 -0500

    Search input screen added

---
+++ app/src/main/java/uta/com/search/SearchInput.java
@@ -0,0 +1,20 @@
+package uta.com.search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import uta.com.studentcenter.R;
+
+/**
+ * Created by vivekraveendran on 7/7/2015.
+ */
+public class SearchInput extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_input);
+    }
+}
