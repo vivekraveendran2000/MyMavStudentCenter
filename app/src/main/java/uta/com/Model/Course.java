d69a498dba6e9d6fa659b43041cdeb63b45896de
vivek <vivekraveendran2000@gmail.com>
2015-07-11 12:43:38 -0500

    added course model

---
+++ app/src/main/java/uta/com/Model/Course.java
@@ -0,0 +1,80 @@
+package uta.com.Model;
+
+/**
+ * Created by vivekraveendran on 7/10/2015.
+ */
+public class Course {
+
+    String course_name;
+    String course_strength;
+    String course_time;
+    String room_no;
+    String start_date;
+    String end_date;
+    String course_num;
+    String instructor_name;
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_strength() {
+        return course_strength;
+    }
+
+    public void setCourse_strength(String course_strength) {
+        this.course_strength = course_strength;
+    }
+
+    public String getCourse_time() {
+        return course_time;
+    }
+
+    public void setCourse_time(String course_time) {
+        this.course_time = course_time;
+    }
+
+    public String getRoom_no() {
+        return room_no;
+    }
+
+    public void setRoom_no(String room_no) {
+        this.room_no = room_no;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getCourse_num() {
+        return course_num;
+    }
+
+    public void setCourse_num(String course_num) {
+        this.course_num = course_num;
+    }
+
+    public String getInstructor_name() {
+        return instructor_name;
+    }
+
+    public void setInstructor_name(String instructor_name) {
+        this.instructor_name = instructor_name;
+    }
+}
