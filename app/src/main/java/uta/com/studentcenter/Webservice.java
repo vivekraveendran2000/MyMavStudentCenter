83fd10d23ee9bd509fadf28c49b970ebe8b32664
vivek <vivekraveendran2000@gmail.com>
2015-07-10 22:40:00 -0500

    Search webservice added

--- app/src/main/java/uta/com/studentcenter/Webservice.java
+++ app/src/main/java/uta/com/studentcenter/Webservice.java
@@ -58,4 +58,50 @@ public class Webservice {
         }
         return 1;
     }
+
+
+
+    public static int Search(String department, String term, String courseNumber, String restriction){
+
+        try {
+
+            URL url = new URL("http://omega.uta.edu/~sxa6933/StudentCenter/search.php");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            String urlParameters = "dept_name=" + "CSE" + "&course_num=" + "5000" + "&restriction=" + ">" +"&term=" + "Fall 2015" ;
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Length", "" +
+                    Integer.toString(urlParameters.getBytes().length));
+            urlConnection.setRequestProperty("Content-Language", "en-US");
+
+            urlConnection.setUseCaches (false);
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream ( urlConnection.getOutputStream ());
+            wr.writeBytes(urlParameters);
+
+            wr.flush ();
+            wr.close ();
+
+            //Get Response
+            InputStream is = urlConnection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while((line = rd.readLine()) != null) {
+                response.append(line);
+                //response.append('\r');
+            }
+            rd.close();
+            String output = response.toString();
+            JSONObject jsonObject = new JSONObject(output);
+            System.out.println(output);
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+        return 1;
+    }
 }
