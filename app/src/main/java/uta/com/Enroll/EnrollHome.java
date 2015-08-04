b02f4c7abc671439f61c4d7892e7ded93884bd78
vivek <vivekraveendran2000@gmail.com>
2015-08-04 18:55:04 -0500

    view schedule implemented, enroll flow from home screen implemented

--- app/src/main/java/uta/com/Enroll/EnrollHome.java
+++ app/src/main/java/uta/com/Enroll/EnrollHome.java
@@ -1,18 +1,18 @@
 package uta.com.Enroll;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-
-import uta.com.Model.Course;
+import uta.com.Cart.ViewCart;
 import uta.com.studentcenter.R;
 import uta.com.studentcenter.Webservice;
 
@@ -23,6 +23,7 @@ public class EnrollHome extends Activity implements View.OnClickListener{
 
     Button enrollBtn, viewScheduleBtn;
     Context context;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +48,13 @@ public class EnrollHome extends Activity implements View.OnClickListener{
 
         if (v.equals(viewScheduleBtn)){
 
+            progressDialog = ProgressDialog.show(context, "Schedule", "Retreiving schedule ..", true);
             new GetScheduleBackground().execute("");
 
-        }else{
+        }else if(v.equals(enrollBtn)){
 
+            progressDialog = ProgressDialog.show(context, "Cart", "Retrieving cart items ..", true);
+            new GetCartBackground().execute("");
 
         }
     }
@@ -81,21 +85,76 @@ public class EnrollHome extends Activity implements View.OnClickListener{
         protected void onPostExecute(String result) {
 
             try {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        progressDialog.dismiss();
+                        if (schedule.equals("failed")){
+
+                        }else{
+
+                            Intent viewScheduleIntent = new Intent(EnrollHome.this, ViewSchedule.class);
+                            viewScheduleIntent.putExtra("result",schedule);
+                            startActivity(viewScheduleIntent);
+                        }
+                    }
+                }, 1000);
+
+            }catch (Exception e){
+
+            }
+        }
+    }
+
+    class GetCartBackground extends AsyncTask<String, String, String> {
+
+        private Exception exception;
+        String cartDetails;
+        String term;
+        String netId;
+
+        protected String doInBackground(String... urls) {
+            try {
+
+                SharedPreferences prefs = context.getSharedPreferences(
+                        "studentcenter", Context.MODE_PRIVATE);
+                term = prefs.getString("search_term","");
+                netId = prefs.getString("net_id","");
+
+                cartDetails = Webservice.viewCart(netId, term);
+
+            } catch (Exception e) {
+                this.exception = e;
+            }
+            return cartDetails;
+        }
+
+        protected void onPostExecute(String result) {
+
+            try {
 
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
 
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressDialog.dismiss();
+                            }
+                        });
+                        if (cartDetails.equals("failed")){
 
-                        if (schedule.equals("failed")){
 
-
+                            Toast.makeText(getApplicationContext(), "Cart empty",
+                                    Toast.LENGTH_SHORT).show();
                         }else{
 
-
+                            Intent viewCartIntent = new Intent(EnrollHome.this, ViewCart.class);
+                            viewCartIntent.putExtra("result", cartDetails);
+                            startActivity(viewCartIntent);
                         }
-
-
                     }
                 }, 1000);
 
---
+++ app/src/main/java/uta/com/Enroll/ViewSchedule.java
@@ -0,0 +1,200 @@
+package uta.com.Enroll;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import uta.com.Model.Course;
+import uta.com.search.SearchSubjectDetails;
+import uta.com.studentcenter.R;
+
+/**
+ * Created by vivekraveendran on 8/4/2015.
+ */
+public class ViewSchedule extends Activity implements View.OnClickListener{
+
+    ImageButton backBtn;
+    Context context;
+    ListView listView;
+    ArrayList<Course> scheduleCourses;
+    Course tempCourse;
+    JSONObject cartJson;
+    ProgressDialog progressDialog;
+    String result;
+    ListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_schedule);
+
+        context = this;
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            result = extras.getString("result");
+        }
+        initData(result);
+        initViews();
+    }
+
+    void initData(String result){
+
+        try {
+
+            JSONArray mJsonArray = new JSONArray(result);
+            scheduleCourses = new ArrayList<Course>(mJsonArray.length());
+            JSONObject mJsonObject = new JSONObject();
+            for (int i = 0; i < mJsonArray.length(); i++) {
+
+                mJsonObject = mJsonArray.getJSONObject(i);
+                tempCourse = new Course();
+                if(mJsonObject.getString("course_name") != null) {
+                    tempCourse.setCourse_name(mJsonObject.getString("course_name"));
+                }
+
+                if(mJsonObject.getString("unique_code") != null) {
+                    tempCourse.setUniqueNumber(mJsonObject.getString("unique_code"));
+                }
+
+                if(mJsonObject.getString("course_strength") != null) {
+                    tempCourse.setCourse_strength(mJsonObject.getString("course_strength"));
+                }
+                if(mJsonObject.getString("course_time") != null) {
+                    tempCourse.setCourse_time(mJsonObject.getString("course_time"));
+                }
+                if(mJsonObject.getString("room_no") != null) {
+                    tempCourse.setRoom_no(mJsonObject.getString("room_no"));
+                }
+                if(mJsonObject.getString("start_date") != null) {
+                    tempCourse.setStart_date(mJsonObject.getString("start_date"));
+                }
+                if(mJsonObject.getString("end_date") != null) {
+                    tempCourse.setEnd_date(mJsonObject.getString("end_date"));
+                }
+                if(mJsonObject.getString("course_num") != null) {
+                    tempCourse.setCourse_num(mJsonObject.getString("course_num"));
+                }
+                if(mJsonObject.getString("instructor_name") != null) {
+                    tempCourse.setInstructor_name(mJsonObject.getString("instructor_name"));
+                }
+
+                scheduleCourses.add(tempCourse);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void initViews(){
+
+        backBtn = (ImageButton) findViewById(R.id.btn_view_schedule_back);
+        backBtn.setOnClickListener(this);
+        listView = (ListView) findViewById(R.id.view_schedule_list);
+        adapter = new ListAdapter(scheduleCourses);
+        listView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.equals(backBtn)){
+            finish();
+        }
+    }
+
+    public class ListAdapter extends BaseAdapter implements  View.OnClickListener{
+
+        private  ArrayList<Course> courses;
+
+        ListAdapter(ArrayList<Course> courses) {
+            this.courses = courses;
+        }
+
+        @Override
+        public int getCount() {
+            return courses.size();
+        }
+
+        @Override
+        public Course getItem(int position) {
+            return courses.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View rowView;
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            rowView = inflater.inflate(R.layout.search_list_item, null);
+            TextView courseNumber = (TextView) rowView.findViewById(R.id.txt_search_list_course_number);
+            TextView courseName = (TextView) rowView.findViewById(R.id.txt_search_list_course_name);
+            TextView instructor = (TextView) rowView.findViewById(R.id.txt_search_list_instructor);
+            ImageButton arrowBtn = (ImageButton) rowView.findViewById(R.id.btn_list_item_select);
+            arrowBtn.setOnClickListener(new ClickHandler(position));
+
+            Course tempCs = getItem(position);
+            courseNumber.setText(tempCs.getCourse_num());
+            courseName.setText(tempCs.getCourse_name());
+            instructor.setText(tempCs.getInstructor_name());
+
+
+            return rowView;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    public class ClickHandler implements View.OnClickListener{
+
+        int position;
+
+        ClickHandler(int position){
+
+            this.position = position;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+//            Course course = scheduleCourses.get(position);
+//            Intent subjectDetailIntent = new Intent(context, SearchSubjectDetails.class);
+//            subjectDetailIntent.putExtra("name",course.getCourse_name());
+//            subjectDetailIntent.putExtra("unique_code",course.getUniqueNumber());
+//            subjectDetailIntent.putExtra("number",course.getCourse_num());
+//            subjectDetailIntent.putExtra("strength",course.getCourse_strength());
+//            subjectDetailIntent.putExtra("time",course.getCourse_time());
+//            subjectDetailIntent.putExtra("end_date",course.getEnd_date());
+//            subjectDetailIntent.putExtra("instructor",course.getInstructor_name());
+//            subjectDetailIntent.putExtra("room", course.getRoom_no());
+//            subjectDetailIntent.putExtra("start_date",course.getStart_date());
+//            subjectDetailIntent.putExtra("coming_from", "view_cart");
+//
+//            startActivityForResult(subjectDetailIntent,1);
+        }
+    }
+}
